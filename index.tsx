c21b8a170e92de9598ef4df68e98ff9ae88c6819
Mika <mturki@users.noreply.github.com>
2019-03-27 15:42:59 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./src/App"
 
 console.log("Coucou depuis tsx!")
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+ReactDOM.render(<p>Hello World</p>, document.getElementById("root"))
